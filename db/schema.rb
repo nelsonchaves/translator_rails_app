e3a8f58ff513b8474a609b0df9c756bd35aa2155
Nelson Chaves <nelsonchavespro@gmail.com>
2020-06-09 16:02:01 -0700

    Rename value to content

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_08_221720) do
+ActiveRecord::Schema.define(version: 2020_06_09_223339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "words", force: :cascade do |t|
-    t.string "value"
+    t.string "content"
     t.string "language"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
--- spec/factories/word.rb
+++ spec/factories/word.rb
@@ -1,7 +1,7 @@
 # This will guess the User class
 FactoryBot.define do
   factory :word do
-    value { 'cat' }
+    content { 'cat' }
     language { 'english' }
   end
 end
