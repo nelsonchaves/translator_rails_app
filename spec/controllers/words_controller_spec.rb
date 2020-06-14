051939772c7ba142cf01a2f417a9e1ef13f8a0df
Nelson Chaves <nelsonchavespro@gmail.com>
2020-06-14 12:53:31 -0700

    Fix Specs

--- spec/controllers/words_controller_spec.rb
+++ spec/controllers/words_controller_spec.rb
@@ -162,15 +162,13 @@ RSpec.describe WordsController, type: :controller do
   describe 'GET edit' do
     subject { get :edit, params: params }
 
-      let(:params) do
+    let(:params) do
         {id: word.id}
-      end
-
-      let!(:word) { create(:word) }
-
-      context 'when user is signed in' do
-        let(:user) { create(:user) }
-
+    end
+    let!(:word) { create(:word, user: user) }
+    let(:user) { create(:user) }
+    
+    context 'when user is signed in' do
       before do
         sign_in(user)
         subject
@@ -200,13 +198,12 @@ RSpec.describe WordsController, type: :controller do
   describe "PUT update" do
     subject { put :update, params: params }
 
-    let!(:word) { create(:word, content: 'cat', language: language_1) }
+    let!(:word) { create(:word, user: user, content: 'cat', language: language_1) }
     let!(:language_1) { create(:language, name: 'English') }
     let!(:language_2) { create(:language, name: 'Polish') }
+    let(:user) { create(:user) }
 
     context 'when user is signed in' do
-      let(:user) { create(:user) }
-
       before do
         sign_in(user)
       end
@@ -264,11 +261,9 @@ RSpec.describe WordsController, type: :controller do
   describe "DELETE Destroy" do
     subject { delete :destroy, params: params }
 
-    let!(:word) { create(:word) }
-
+    let!(:word) { create(:word, user: user) }
+    let(:user) { create(:user) }
     context 'when user is signed in' do
-      let(:user) { create(:user) }
-
       before do
         sign_in(user)
       end
